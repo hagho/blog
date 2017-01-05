531b64b15753273949621800ccedf064fd580fcf
huangchao <869158286@qq.com>
2017-01-05 21:02:40 +0800

    add function

--- article/urls.py
+++ article/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home),
-    url(r'^/(?P<my_args>\d+)/$', views.detail, name='detail'),
+    url(r'^$', views.home, name = 'home'),
+    url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
     url(r'^test/$', views.test),
 ]
--- article/views.py
+++ article/views.py
@@ -3,18 +3,27 @@ from datetime import datetime
 
 # Create your views here.
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from models import Article
 
 def home(request):
     post_list = Article.objects.all()  
     return render(request, 'home.html', {'post_list' : post_list})
-
+    
+'''
 def detail(request, my_args):
     post = Article.objects.all()[int(my_args)]
     str = ("title = %s, category = %s, date_time = %s, content = %s" 
         % (post.title, post.category, post.date_time, post.content))
     return HttpResponse(str)
+'''
+
+def detail(request, id):
+    try:
+        post = Article.objects.get(id = str(id))
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'post.html', {'post' : post})
 
 def test(request) :
     return render(request, 'test.html', {'current_time': datetime.now()})
